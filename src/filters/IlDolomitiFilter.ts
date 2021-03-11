536204276cf2b70efcea31aa997d82c6b3bfeedb
Matteo Contrini <matteo@contrini.it>
2021-03-11 13:07:30 +0100

    Handle missing guid ildolomiti

--- src/filters/IlDolomitiFilter.ts
+++ src/filters/IlDolomitiFilter.ts
@@ -3,13 +3,9 @@ import IFeedFilter from "./IFeedFilter";
 
 export default class IlDolomitiFilter implements IFeedFilter {
     public execute(items: Item[]): void {
-        //  <guid isPermaLink="false">Consegnate al Comune 1.240 firme23300</guid>
-        // becomes
-        //  <guid isPermaLink="false">23300</guid>
         for (const post of items) {
-            const match = post.guid.match(/\D(\d+)$/);
-            if (match) {
-                post.guid = match[1];
+            if (!post.guid) {
+                post.guid = post.link;
             }
         }
     }
